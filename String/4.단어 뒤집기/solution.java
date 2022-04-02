875a8d94d95dd295d5cf38f2929205e6f49186cd
KimH4nKyul <alskim93@kakao.com>
2022-04-03 03:56:30 +0900

    4.단어 뒤집기

---
+++ String/4.단어 뒤집기/solution.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+
+class Main {
+    public static String reverse(char[] charArray) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = charArray.length; 0 < i; i--) {
+            sb.append(charArray[i - 1]);
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    public static List<String> inputStrings(int num, BufferedReader br) throws IOException {
+        List<String> lists = new ArrayList<>();
+        // int n = Character.getNumericValue(num);
+
+        for (int i = 0; i < num; i++) {
+            char[] chars = br.readLine().toCharArray();
+            lists.add(reverse(chars));
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        List<String> lists = inputStrings(num, br);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (String s : lists) {
+            bw.append(s);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
